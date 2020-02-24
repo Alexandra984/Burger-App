2e135d61ca00d755bcb63d9025b499013b912c20
Alexandra Grecu <38529225+Alexandra984@users.noreply.github.com>
2020-02-25 01:00:15 +0200

    Implement the toolbar.

---
+++ burger-app/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const sideDrawer = (props) => {
+    // ... 
+    return(
+        <div>
+            <NavigationItems />
+
+        </div>
+    );
+};
+
+export default sideDrawer;
