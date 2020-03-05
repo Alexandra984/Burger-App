97c4f69580af61163344cb2e177987df7fab24c1
Alexandra Grecu <38529225+Alexandra984@users.noreply.github.com>
2020-03-05 22:59:38 +0200

    Adding form.

--- burger-app/src/components/UI/Button/Button.js
+++ burger-app/src/components/UI/Button/Button.js
@@ -4,7 +4,7 @@ import classes from './Button.css';
 const button = (props) => (
 <button 
     className={[classes.Button, classes[props.btnType]].join(' ')} // e un array de stringuri si de fapt trebuie doar un singur string !!!
-
+    disabled={props.disabled}
     onClick={props.clicked}>{props.children}</button>
 );
 
